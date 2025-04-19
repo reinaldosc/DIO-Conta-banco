22d2be11189f96879bc4018f5dd58b58aaaab049
Reinaldo <reinaldo.silva108@etec.sp.gov.br>
2025-04-19 15:25:23 -0300

    finalizing the main class 'contaTerminal'

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        String nomeCliente, agencia;
+        int numero;
+        double saldo;
+
+        try(Scanner scanner = new Scanner(System.in).useLocale(Locale.US)){
+
+            System.out.println("Digite o seu nome");
+            nomeCliente = scanner.next();
+
+            System.out.println("Digite o numero da conta");
+            numero = scanner.nextInt();
+
+            System.out.println("Digite a agencia da conta");
+            agencia = scanner.next();
+
+            System.out.println("Digite o saldo da conta");
+            saldo = scanner.nextDouble();
+        }
+
+        Conta conta = new Conta(numero, saldo, agencia, nomeCliente);
+
+        System.out.println(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", conta.getNomeCliente(), conta.getAgencia(), conta.getNumero(), conta.getSaldo()));
     }
 }
